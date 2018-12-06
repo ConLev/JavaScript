dc7c16cb30bcefa9c95a38b6dd3dc230040f9f72
ConLev <levconst@gmail.com>
2018-12-06 14:35:20 +0300

    work on the bugs

--- lesson_5/1.js
+++ lesson_5/1.js
@@ -7,6 +7,17 @@ const chess = {
     gameContainerEl: document.getElementById('game'),
 
     /**
+     * Определяет является ли ячейка черной.
+     * @param {int} rowNum Номер в строке.
+     * @param {int} colNum Номер в колонке.
+     * @returns {boolean} true, если ячейка должна быть черной, иначе false.
+     */
+    isCellIsBlack(rowNum, colNum) {
+        return rowNum % 2 !== 0 && colNum % 2 === 0 && rowNum < 9 ||
+            rowNum % 2 === 0 && colNum % 2 !== 0 && rowNum > 0;
+    },
+
+    /**
      * Метод отображения карты (игрового поля).
      */
     renderMap() {
@@ -29,34 +40,20 @@ const chess = {
                 const cell = document.createElement('td');
                 if (row === 0 && col > 0 && col < 9 || row === 9 && col > 0 && col < 9) {
                     cell.innerText = cellInnerLet[col - 1];
-                    cell.bgColor = '#eee';
+                    cell.style.backgroundColor = '#eee';
                 } else if (col === 0 && row > 0 && row < 9 || col === 9 && row > 0 && row < 9) {
-                    // noinspection JSValidateTypes
-                    cell.innerText = cellInnerDig[row - 1];
-                    cell.bgColor = '#eee';
+                    cell.innerText = cellInnerDig[row - 1].toString();
+                    cell.style.backgroundColor = '#eee';
                 } else if (this.isCellIsBlack(row, col)) {
-                    cell.bgColor = '#777';
+                    cell.style.backgroundColor = '#777';
                 } else {
-                    cell.bgColor = '#eee';
+                    cell.style.backgroundColor = '#eee';
                 }
                 // Записываем ячейку в массив ячеек.
                 this.cellElements.push(cell);
                 // Добавляем ячейку в текущую строку.
                 trElem.appendChild(cell);
             }
-        }
-    },
-
-    /**
-     * Определяет является ли ячейка черной.
-     * @param {int} rowNum Номер в строке.
-     * @param {int} colNum Номер в колонке.
-     * @returns {boolean} true, если ячейка должна быть черной, иначе false.
-     */
-    isCellIsBlack(rowNum, colNum) {
-        if (rowNum % 2 !== 0 && colNum % 2 === 0 && rowNum < 9 ||
-            rowNum % 2 === 0 && colNum % 2 !== 0 && rowNum > 0) {
-            return true;
         }
     },
 };
